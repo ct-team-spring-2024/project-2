1aeecb472f3bdc7fe0fc2e95b22b004e36fbbb87
Mohammad Broughani <gitmbroughani81@gmail.com>
2025-04-08 14:40:12 +0330

    ~

--- frontend/cmd/main.go
+++ frontend/cmd/main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"github.com/sirupsen/logrus"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 
 func main() {
-	logger := logrus.New()
-	logger.SetLevel(logrus.DebugLevel)
-	logger.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetFormatter(&logrus.TextFormatter{})
 
-	logger.Debug("This is a debug message")
+	logrus.Debug("This is a debug message")
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "./static")
+
+	// router.GET("/", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, "index.html", nil)
+	// })
+	cnt := 0
+	router.GET("/", func(c *gin.Context) {
+		cnt = cnt + 1
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"counter": cnt,
+		})
+	})
+
+	router.Run(":8080")
 }
