69848a09283a537ae27bd52a92831ee6b0b670b0
Amirahmad Shafiee <freceerwin@gmail.com>
2025-04-20 04:52:57 +0330

    database interface updated

--- goforces/internal/db/interface.go
+++ goforces/internal/db/interface.go
@@ -1,11 +1,17 @@
 package db
 
 import (
-	"oj/goforces/internal"
 	"oj/goforces/internal/models"
 )
 
+var DB Database
+
 type Database interface {
-	GetUserSubmission(internal.User) []models.Submission
-	AddSubmission(models.Submission) error
+	GetUserSubmission(userID int) []models.Submission
+	AddSubmission(s models.Submission) error
+	GetUserByID(userID int) (*models.User, error)
+	CreateUser(user models.User) error
+	GetProblemByID(problemID int) (*models.Problem, error)
+	CreateProblem(problem models.Problem) error
+	UpdateProblemStatus(problemID int, status models.ProblemStatus) error
 }
